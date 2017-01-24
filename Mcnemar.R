2a6f88f0afaa17b06681a4581466fba5653e668f
turdlez <ciadams@iastate.edu>
2017-01-24 17:48:55 -0600

    Added the REAL stuff

--- Mcnemar.R
+++ Mcnemar.R
@@ -59,3 +59,34 @@ chisq.test(x4)
 aggregate(TD$Amp, list(TD$Pond), mean)
 #Average of Extractions
 aggregate(TD$Amp, list(TD$Extraction), mean)
+
+# ------- Actual McNemar's ------------ #
+
+Test<- read.csv("McNemarData.csv")
+Test$Date..summer..15.<-NULL
+table1<-table(Test)
+
+table1
+#SA0 GMF0 = 59 
+15+12+18+14
+#SA0 GMF1 = 34
+9+11+6+8
+#SA1 GMF0 = 10
+3+2+2+3
+#SA1 GMF1 = 13
+2+4+3+4
+x5<- matrix(c(59, 34, 10, 13), byrow = TRUE, 2, 2)
+mcnemar.test(x5,y = NULL, correct = TRUE)
+
+#Without Pond A
+#SA0 GMF0 = 44 
+12+18+14
+#SA0 GMF1 = 25
+11+6+8
+#SA1 GMF0 = 7
+2+2+3
+#SA1 GMF1 = 11
+4+3+4
+x6<-matrix(c(44, 23, 7, 11), byrow = TRUE, 2, 2)
+mcnemar.test(x6)
+chisq.test(x6)
