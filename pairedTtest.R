57b9cdc58c991590f745809136f2aa6726a5f854
turdlez <ciadams@iastate.edu>
2017-02-09 13:53:57 -0600

    Adam did some stuff

--- pairedTtest.R
+++ pairedTtest.R
@@ -8,3 +8,30 @@ head(PTT)
 
 t.test(PTT$GMF, PTT$SA, paired=TRUE)
 #t = 1.7984, df = 141, p-value = 0.07425, paired t-test = not significant
+
+library(reshape2)
+PTT <- na.omit(PTT)
+PTT.m <- melt(PTT, id.vars=c("JDATE", "Pond"), measure.vars = c("GMF", "SA"))
+m <- lm(value ~ Pond + variable*JDATE, data=PTT.m)
+m.diff <- lm(Diff ~ Pond + JDATE, data=PTT)
+summary(m)
+summary(m.diff)
+res <- residuals(m.diff)
+par(mfrow=c(2,2))
+acf(res[PTT$Pond=="A"])
+acf(res[PTT$Pond=="B"])
+acf(res[PTT$Pond=="C"])
+acf(res[PTT$Pond=="D"])
+par(mfrow=c(1,1))
+
+qplot(JDATE, value, geom="point", data=PTT.m, facets=variable~Pond)
+qplot(value, geom="histogram", data=PTT.m, facets=variable~Pond)
+#If we (1) replace the negative and low values with the lowest detected value for each method respectively and then log the values, we can compare methods and do a paired t-test that way... 
+#We can also view histograms of the vaues for each pond by each method
+smurf <- PTT
+smurf$GMF <- pmax(smurf$GMF, 0.4)
+smurf$SA  <- pmax(smurf$SA, 0.8)
+smurf <- melt(smurf, id.vars=c("JDATE", "Pond"), measure.vars = c("GMF", "SA"))
+qplot(log(value), geom="histogram", data=smurf, facets=variable~Pond, binwidth=1/3)
+##Here's the t-test
+t.test(value~variable, paired = TRUE, data=smurf)
