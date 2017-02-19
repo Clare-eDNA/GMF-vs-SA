7c7a0e6e8d0b7cce1905ff928b0ff2c692a94899
turdlez <ciadams@iastate.edu>
2017-02-19 14:40:56 -0600

    ran some linear models to confirm to myself what was going on

--- pairedTtest.R
+++ pairedTtest.R
@@ -4,7 +4,7 @@ head(PTT)
 PTT$Diff=PTT$GMF-PTT$SA
 head(PTT)
 
-#Remove all NA columns
+########### Remove all NA columns ############
 
 t.test(PTT$GMF, PTT$SA, paired=TRUE)
 t.test(smurf$GMF, smurf$SA, paired=TRUE)
@@ -57,7 +57,7 @@ summary(m2)
 #Pond B is different than Pond A, and a potentially signficant interaction between pond and SA vs GMF extraction
 m3<- glm(value~Pond*JDATE, data=smurf)
 summary(m3)
-####### But, if we look at the differences############################################
+####### But, if we look at the differences without adding .4 or .8 ######################
 
 m4 <- glm(Diff~JDATE, data=PTT)
 summary(m4)
@@ -68,3 +68,43 @@ summary(m5)
 #Pond B is different than Pond A, and a potentially signficant interaction between pond and SA vs GMF extraction
 m6<- glm(Diff~Pond*JDATE, data=PTT)
 summary(m6)
+
+##################################################
+####### Now, with adding 1.91 to everything #######
+##################################################
+
+head(PTT)
+summary(PTT)
+PTT$GMF1<- PTT$GMF+1.91
+PTT$SA1<- PTT$SA+1.91
+summary(PTT)
+
+#Log the data
+PTT$LogGMF<- log(PTT$GMF1)
+PTT$LogSA<-log(PTT$SA1)
+summary(PTT)
+
+#Run a T-test on the data
+t.test(PTT$LogGMF, PTT$LogSA, data=PTT, paired=TRUE) #Not a significant difference, p-val = 0.3
+
+#Tried to see if there was an effect of date
+#First reshaped the data
+PTT2 <- melt(PTT, id.vars=c("JDATE", "Pond"), measure.vars = c("LogGMF", "LogSA"))
+summary(PTT2)
+
+#ran some gaussian glms
+m7 <- glm(value~JDATE*variable, data=PTT2, family=gaussian) #Significant effect of time
+summary(m7)
+m8 <- glm(value~Pond*variable, data=PTT2, family=gaussian)
+summary(m8)
+m9 <- glm(value~Pond*JDATE, data=PTT2, family=gaussian)
+summary(m9)
+
+#But looking at differences
+summary(PTT2)
+summary(PTT)
+PTT$Diff1<- (PTT$LogGMF-PTT$LogSA)
+m10<- glm(Diff1~JDATE, data=PTT, family=gaussian) #No effect of date
+summary(m10)
+m11 <- glm(Diff1~Pond*JDATE, data=PTT, family=gaussian) #No effect of anything
+summary(m11)
